06840757ed1d6a18058e9d742bb312f5218a40ca
Tristan Neumann <tristan.neumann@hotmail.com>
2018-12-08 23:53:29 -0800

    Fixed port env name;

--- server/index.js
+++ server/index.js
@@ -10,13 +10,8 @@ app.use(morgan('[:date[clf]] :method :status :url :res[content-length] - :respon
 app.use(parser.urlencoded({extended: false}));
 
 app.use('/', express.static(path.join(__dirname, '../dist')));
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
-// });
 
-
-
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>console.log(`server open on port: ${port}`));
 
