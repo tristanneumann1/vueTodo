65f22b13352ade9662048812e42d362dfbfd7f9b
Tristan Neumann <tristan.neumann@hotmail.com>
2019-01-14 14:23:06 -0800

    email service with sendgrid;

--- server/router.js
+++ server/router.js
@@ -1,28 +1,32 @@
 const router = require('express').Router();
-const nodemailer = require('nodemailer');
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'tristanneumann0@gmail.com',
-    pass:process.env.password,
-  }
-});
-const footer = '\n\n\n';
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'test@example.com',
+  from: 'test@example.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+};
+sgMail.send(msg);
+const footer = '\n\n\nmessage from: ';
+
 
 router.route('/email')
   .post((req, res) => {
     const {nom, prenom, email, message} = req.body;
-    const mailOptions = {
+    const msg = {
+      to: 'tristanneumann1@gmail.com',
       from: email,
-      to: 'Travauxchezmoi78@gmail.com',
-      subject: `email from site: ${prenom} ${nom}`,
+      subject: 'message from travaux chez moi: ' + nom + ' ' + prenom,
       text: message + footer + email,
     };
-    transporter.sendMail(mailOptions, (err, info) => {
+    sgMail.send(msg, (err, info) => {
       if(err) {
         console.log(err);
         res.status(400).send('Email error occured');
       } else {
+        console.log(info);
         res.status(202).send(info);
       }
     });
